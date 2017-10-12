d63feac250ebe9d0868eda7b2752c6d14eee4c45
Marc Bachmann <marc.brookman@gmail.com>
2017-10-12 22:45:20 +0200

    feat: Use json logs instead of unparseable list of properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ sed -i.original "s|{{BACKEND_HOST}}|${BACKEND_HOST}|g" $VARNISH_CONFIG
 sed -i.original "s|{{BACKEND_PORT}}|${BACKEND_PORT}|g" $VARNISH_CONFIG
 
 varnishd -f $VARNISH_CONFIG -s malloc,$VARNISH_CACHE_SIZE -a 0.0.0.0:${VARNISH_PORT} -T 0.0.0.0:2000
-varnishlog
+varnishncsa -F '{"@timestamp":"%{%Y-%m-%dT%H:%M:%S%z}t","method":"%m","url":"%U","remote_ip":"%h","x_forwarded_for":"%{X-Forwarded-For}i","cache":"%{Varnish:handling}x","bytes":%b,"duration_usec":%D,"status":%s,"request":"%r","ttfb":"%{Varnish:time_firstbyte}x","referrer":"%{Referrer}i","user_agent":"%{User-agent}i"}'
