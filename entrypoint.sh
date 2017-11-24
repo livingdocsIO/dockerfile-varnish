15f01df1830f03dc4d874eb6c5e24f07145c87c4
Marc Bachmann <marc.brookman@gmail.com>
2017-11-24 13:58:13 +0100

    Add `sleep 1` to varnish log script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 /bin/varnish-reload-synconly
-NO_LOG_PREFIX=first exitd /bin/varnish-logs /bin/varnish /bin/varnish-reload-watch /bin/prometheus-varnish-exporter
+EXITD_NO_LOG_PREFIX=first exitd /bin/varnish-logs /bin/varnish /bin/varnish-reload-watch /bin/prometheus-varnish-exporter
