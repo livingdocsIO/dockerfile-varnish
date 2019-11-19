232f6eff456f0a3bc61197846c539f03686d1e5a
Marc Bachmann <marc.brookman@gmail.com>
2019-11-19 10:47:21 +0100

    feat: Support a Follow-Location header to serve content on a different url

--- server.js
+++ server.js
@@ -26,8 +26,24 @@ const server = http.createServer(function (req, res) {
     return res.end()
   }
 
+  if (req.url.startsWith('/regular-redirect')) {
+    res.writeHead(302, {'Content-Type': 'text/html', Location: '/some-fake-page-to-redirect'})
+    res.write('302 Found')
+    return res.end()
+  }
+
+  if (req.url.startsWith('/follow-redirect')) {
+    res.writeHead(302, {
+      'Content-Type': 'text/html',
+      Location: `/fake-redirect/${req.url.replace('/follow-redirect/', '')}`,
+      'Follow-Location': 'true'
+    })
+    res.write('302 Found')
+    return res.end()
+  }
+
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write('Success Page')
+  res.write(`Success Page ${req.url}`)
   res.end()
 })
 
